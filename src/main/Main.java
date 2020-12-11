0279ca6578903b5e6f3952249f3a95dd1d0e26a2
pani-marinka <pani_marinka@ukr.net>
2020-12-11 06:19:38 +0200

    initPanarina

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,8 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        new Calc().setVisible(true);
+        new Calc().setVisible(true;
+        //calcultor
     }
     
 }
